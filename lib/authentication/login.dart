e380c381e9b72b455ac58827aa2c25a5889a9c62
jay67nie <jeromessenyonjo@outlook.com>
2022-09-21 18:57:59 +0300

    Login and Sign Up logic

---
+++ lib/authentication/login.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Login{
+  Login({required this.email, required this.password});
+
+  String email;
+  String password;
+
+
+  Future<String?> login() async {
+
+    var myAuth = FirebaseAuth.instance;
+    try {
+      await myAuth.signInWithEmailAndPassword(email: email, password: password);
+      return 'Success';
+    }
+    on FirebaseAuthException catch (e) {
+      if(e.code == 'user-not-found') {
+        return 'This account does not exist.';
+      } else if(e.code == 'wrong-password') {
+        return 'The password is incorrect';
+      }
+      else {
+        return e.message;
+      }
+    }
+    catch(e){
+      return e.toString();
+    }
+  }
+
+
+
+}
---
+++ lib/authentication/sign_up.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUp{
+  SignUp({required this.email, required this.password});
+
+  String email;
+  String password;
+
+
+  Future<String?> signUp() async {
+
+    var myAuth = FirebaseAuth.instance;
+    try {
+      await myAuth.createUserWithEmailAndPassword(
+          email: email, password: password)
+          .then((value) =>
+          myAuth.signInWithEmailAndPassword(email: email, password: password))
+          .then((value) => myAuth.currentUser!.sendEmailVerification());
+          return 'Success';
+    }
+    on FirebaseAuthException catch (e) {
+      if(e.code == 'weak-password') {
+        return 'The password is too weak.';
+      } else if(e.code == 'email-already-in-use') {
+        return 'The account already exists for that email';
+      }
+      else {
+        return e.message;
+      }
+    }
+    catch(e){
+      return e.toString();
+    }
+  }
+
+
+
+}
