f4caca1ceef29eac16f9ab717638b4384f467bd3
jay67nie <jeromessenyonjo@outlook.com>
2022-09-25 00:04:58 +0300

    Worked on Login Mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:stuatt/firebase_options.dart';
 
 import 'loginpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
     
     runApp(MyApp());
 }
