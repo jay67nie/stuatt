34f9b86437ff9d7bbd791b36f56436eff509eb18
shanicekakai <pandenorah878@gmail.com>
2022-09-26 16:00:34 +0300

    Merge branch 'master' of https://github.com/jay67nie/stuatt

---
+++ lib/before page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:stuatt/generate.dart';
+
+void main(){
+  runApp(const generatepage());
+}
+
+class generatepage extends StatelessWidget {
+  const generatepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: 'STuAtt',
+        home: myHome(),
+    );
+  }
+}
+
+class myHome extends StatelessWidget{
+  const myHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('StuAtt')
+        ),
+        body:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(child: ElevatedButton (
+                        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => StuAtt().myQR()));},
+                        child: const Text("Generate QR Code",
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
+                      ),
+                      ),
+                    ]
+                ),
+              ),
+            ]
+        )
+    );
+  }
+}
