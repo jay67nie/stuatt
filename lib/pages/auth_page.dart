d07459ea7c1aacb8f57da944e32f09470b5198f2
Michelle.atala <atalamchll7@gmail.com>
2022-09-29 15:06:52 +0300

    register page

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -19,9 +19,10 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     if (showLoginPage) {
-      return LoginPage(showRegisterpage: toggleScreens);
+      return LoginPage(showRegisterPage: toggleScreens);
     } else {
       return RegisterPage(showLoginPage: toggleScreens);
     }
   }
 }
+
